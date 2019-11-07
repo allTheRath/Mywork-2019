ad09cb8040369be8375a696c7c82ff79ccc1d166
jay harshadbhai patel <jhpatel15@gmail.com>
2019-11-07 10:36:17 -0600

    Static_Pract

---
+++ Static_Pract/Program.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:82072ff4d7c2935520df0d3f5e850d7a1ad6ab59859a5e8bfacf36f78c053b6d
+size 938
---
+++ Static_Pract/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec6755f38a0fc1a2f6ad521f1f055af3121e804d545332f400b89056c7adb76b
+size 1431
