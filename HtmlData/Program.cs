e33e0c868304630f405b97bfc756d7df446bdd15
jay harshadbhai patel <jhpatel15@gmail.com>
2019-11-07 09:58:16 -0600

    HtmlData

---
+++ HtmlData/Program.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3176d1645e2a5178682a758ed7b605120e33c2c322a87f7e55cbbb74feebd509
+size 1146
---
+++ HtmlData/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3b80291581d28c0067418f9478cd9a023c4966b747b58069d5f13cf7122e5523
+size 1423
