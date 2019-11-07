467183c871f9b1926cce1dcb6fbfa4c9f8bfeff7
jay harshadbhai patel <jhpatel15@gmail.com>
2019-11-07 09:56:08 -0600

    Frontend-DbApp

---
+++ Frontend-DbApp/HtmlCssJs/BaseTag.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6d6a22ca1d6987cf5b72f5612f1669a54a90ac6b5362b3dcdc92d6f084920dac
+size 385
---
+++ Frontend-DbApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:69bbff5869e7ed420a898033da5596935dd13531facaaed8b48dbb387a4534a2
+size 1399
