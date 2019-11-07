b1de3c7855afb78799fe9db845c5de4ac18021c7
jay harshadbhai patel <jhpatel15@gmail.com>
2019-11-07 10:05:56 -0600

    Maths

---
+++ Maths/Program.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f12599835c163415fffb6c111af2bf801b6c7a54d843123769dde91818209613
+size 3229
---
+++ Maths/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5976975c31249919c193a108c2f9f9cfccbdd5b751f271839faf7818224d306a
+size 1417
