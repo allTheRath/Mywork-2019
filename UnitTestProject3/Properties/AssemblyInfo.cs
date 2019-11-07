08c7e52f7ad6950a054ea87fa87dcb4acf3ce002
jay harshadbhai patel <jhpatel15@gmail.com>
2019-11-07 10:43:32 -0600

    UnitTestProject3

---
+++ UnitTestProject3/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:563a0f1f3dfa629ba49c0c6a8521b9abe70499fbc7cebb1941dfe1b7d62d4a01
+size 653
---
+++ UnitTestProject3/UnitTest1.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3bc610147639ee971b66b581d8c737271d4148566a8c71b0f04dec1e57044746
+size 1595
