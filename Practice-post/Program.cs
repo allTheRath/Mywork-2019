e22c6fee0e5d6cece792347c9b1cb013495612a8
jay harshadbhai patel <jhpatel15@gmail.com>
2019-11-07 10:15:26 -0600

    Practice-post

---
+++ Practice-post/Program.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:84e515a4806f6f99738cbd780c18177b2059fdcd60a4b790d0143ff2514b1f14
+size 253
---
+++ Practice-post/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:788b2f8a4144ab80e0c103eae0128acfed6f8cf9a3fb9484378b3b77b6a455f0
+size 1433
