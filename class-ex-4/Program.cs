d8accd253dd78fa42b5ac72c70d3728242c99700
jay harshadbhai patel <jhpatel15@gmail.com>
2019-11-07 09:41:00 -0600

    class-ex-4

---
+++ class-ex-4/Program.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:390028b640001e484bf2769e102e9dff5244181a8cbd8961f7e1c49b0908b5ae
+size 1419
---
+++ class-ex-4/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:10558507586f6223006a170c4a642d2579ae5d80a9df27984ffb9087bc283667
+size 1427
